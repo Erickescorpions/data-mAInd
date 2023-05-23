47460da418499cc4e3f515c12114c87fcd142471
Erick Vazquez <erickescorpions@hotmail.com>
2023-05-23 16:11:55 -0600

    cambiando api

--- back/app/MetricasDistancia.py
+++ back/app/MetricasDistancia.py
@@ -4,36 +4,23 @@ from scipy.spatial.distance import cdist    # Para el cálculo de distancias
 from sklearn.preprocessing import StandardScaler, MinMaxScaler  
 
 class MetricasDistancia():
-    def __init__(self) -> None:
-        self.dataset = "datos_prueba/metricas/Hipoteca.csv"
-        self.data_estandarizada = None
-        self.metrica = 'euclidean'
-        self.distancias = None
-
-    def setDistancias(self, distancias):
-        self.distancias = distancias
-
-    def setDataset(self, dataset: str):
-        pass
-
-    def setDataEstandarizada(self, data_estandarizada):
-        self.data_estandarizada = data_estandarizada
-
-    def setMetrica(self, metrica):
-        self.metrica = metrica
-
     @staticmethod
-    def execute(metricas):         
+    def execute(metrica, dataset="datos_prueba/metricas/Hipoteca.csv"):         
         # ejecucion del algoritmo 
-        data = pd.read_csv(metricas.dataset)
+        data = pd.read_csv(dataset)
 
         # estandarizacion de datos
         estandarizar = StandardScaler()
         # Se calculan la media y desviación y se escalan los datos
         data_estandarizada = estandarizar.fit_transform(data)
 
-        metricas.setDataEstandarizada(data_estandarizada)
+        print(data_estandarizada)
 
         # matriz de distancias
-        dst = cdist(data_estandarizada, data_estandarizada, metric=metricas.metrica)
-        metricas.setDistancias(dst.tolist())
+        dst = cdist(data_estandarizada, data_estandarizada, metric=metrica)
+        dst = dst.tolist()
+
+        return {
+            "sucess": True,
+            "distancias": dst
+        }
--- back/app/app.py
+++ back/app/app.py
@@ -17,8 +17,13 @@ def index():
 '''
 @app.route('/api/apriori', methods=['POST'])
 def executeApriori():
-
     try:
+        if not request.json['confianza'] or not request.json['elevacion'] or not request.json['soporte']:
+            return {
+                "sucess": False,
+                "mensaje": "Es necesario proporcionar los parametros para ejecutar el algoritmo."
+            }
+        
         parametros = {
             "confianza": float(request.json['confianza']),
             "elevacion": float(request.json['elevacion']),
@@ -37,25 +42,24 @@ def executeApriori():
             "message": "Los parametros ['confianza', 'elevacion' y 'soporte'] deben de ser de tipo flotante."
         }
 
-# '''
-#     ruta para agregar un valor a la metrica
-# '''
-# @app.route('/api/metricas/metrica', methods=['POST'])
-# def agregarMetrica():
-#     metricasController = MetricasDistanciaController()
-#     res = metricasController.setMetrica(request)
-#     return jsonify(res)
-
 '''
     ruta para obtener las distancias, recibe el tipo de 
     metrica y el archivo y regresa un json con los datos
     estandarizados y la matriz de distancias
 '''
 # @app.route('/api/metricas', methods=['GET'])
-# def executeMetricas():
-#     metricasController = MetricasDistanciaController()
-#     res = metricasController.execute()
-#     return jsonify(res)
+def executeMetricas():
+    valores_metricas = ['euclidean', 'chebyshev', 'cityblock', 'minkowski']
+    metrica = request.json['metrica']
+    
+    if metrica not in valores_metricas: 
+        return {
+            "sucess": False,
+            "message": f"No se reconoce la metrica {metrica}, por favor envia una metrica que este dentro de los siguiente valores: {valores_metricas}"
+        }
+
+    res = MetricasDistancia.execute(metrica)
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
