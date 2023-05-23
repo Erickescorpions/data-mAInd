f093dedee4aae9b6b65aa3ba77dc9862a4d82988
Erick Vazquez <erickescorpions@hotmail.com>
2023-05-23 16:05:01 -0600

    cambiando api

--- back/app/Apriori.py
+++ back/app/Apriori.py
@@ -4,40 +4,55 @@ import matplotlib.pyplot as plt     # Para la generación de gráficas a partir 
 import apyori
 
 class Apriori():
-    def __init__(self) -> None:
-        self.frecuencia = None
-        self.dataset = "datos_prueba/apriori/movies.csv"
-        self.soporte = None
-        self.elevacion = None
-        self.confianza = None
-        self.reglas = None
+    @staticmethod
+    def execute(parametros, dataset="datos_prueba/apriori/movies.csv"):         
+        #ejecucion del algoritmo 
+        data = pd.read_csv(dataset)
 
-    def setFrecuencia(self, frecuencia):
-        self.frecuencia = {}
+        transacciones = data.values.reshape(-1).tolist()
+        lista = pd.DataFrame(transacciones)
+        lista['Frecuencia'] = 1
+
+        #Se agrupa los elementos
+        lista = lista.groupby(by=[0], as_index=False).count().sort_values(by=['Frecuencia'], ascending=True) #Conteo
+        lista['Porcentaje'] = (lista['Frecuencia'] / lista['Frecuencia'].sum()) #Porcentaje
+        lista = lista.rename(columns={0 : 'Item'})
+        lista = lista.to_numpy().tolist()
+
+        # Convertimos las frecuencias a json
+        frecuencia = {}
         index = 0
         
-        for item in frecuencia:
-            self.frecuencia[index] = {
+        for item in lista:
+            frecuencia[index] = {
                 "pelicula": item[0],
                 "frecuencia": item[1],
                 "porcentaje": item[2]
             }
             index = index + 1
 
-    def setSoporte(self, soporte: float):
-        self.soporte = soporte
+        #Se crea una lista de listas a partir del dataframe y se remueven los 'NaN'
+        data_lista = data.stack().groupby(level=0).apply(list).tolist()
 
-    def setConfianza(self, confianza: float):
-        self.confianza = confianza
+        # Datos de regla de asocicion se obtienen de la clase
+        # por el memonto se los asignamos asi cmo asi
+        soporte_minimo = parametros['soporte']
+        confianza_minima = parametros['confianza']
+        elevacion_minima = parametros['elevacion']
 
-    def setElevacion(self, elevacion: float):
-        self.elevacion = elevacion
+        reglas = apyori.apriori(data_lista, 
+            min_support=soporte_minimo, 
+            min_confidence=confianza_minima, 
+            min_lift=elevacion_minima)  
+        
+        lista_reglas = list(reglas)
+        df_reglas = pd.DataFrame(lista_reglas)
 
-    def setReglasAsociacion(self, reglas):
-        self.reglas = {}
+        #se guardan las reglas en la clase 
+        reglas = {}
         index = 0
 
-        for regla in reglas:
+        for regla in lista_reglas:
             items = []
             parametros = {}
             
@@ -50,57 +65,15 @@ class Apriori():
                 "soporte": regla[1]
             }
 
-            self.reglas[index] = {
+            reglas[index] = {
                 "regla": items,
                 "parametros": parametros
             }
 
             index = index + 1
 
-
-    def setDataset(self, dataset: str):
-        self.dataset = dataset
-
-    def getParametros(self):
         return {
-            'confianza': self.confianza,
-            'elevacion': self.elevacion,
-            'soporte': self.soporte
+            "sucess": True,
+            "frecuencia": frecuencia,
+            "reglas": reglas
         }
-
-    @staticmethod
-    def execute(apriori):         
-        #ejecucion del algoritmo 
-        data = pd.read_csv(apriori.dataset)
-
-        transacciones = data.values.reshape(-1).tolist()
-        lista = pd.DataFrame(transacciones)
-        lista['Frecuencia'] = 1
-
-        #Se agrupa los elementos
-        lista = lista.groupby(by=[0], as_index=False).count().sort_values(by=['Frecuencia'], ascending=True) #Conteo
-        lista['Porcentaje'] = (lista['Frecuencia'] / lista['Frecuencia'].sum()) #Porcentaje
-        lista = lista.rename(columns={0 : 'Item'})
-        lista = lista.to_numpy().tolist()
-        # Se guarda la frecuencia en la clase
-        # aqui
-        apriori.setFrecuencia(lista)
-
-        #Se crea una lista de listas a partir del dataframe y se remueven los 'NaN'
-        data_lista = data.stack().groupby(level=0).apply(list).tolist()
-
-        # Datos de regla de asocicion se obtienen de la clase
-        # por el memonto se los asignamos asi cmo asi
-        soporte_minimo = apriori.soporte
-        confianza_minima = apriori.confianza
-        elevacion_minima = apriori.elevacion
-
-        reglas = apyori.apriori(data_lista, 
-            min_support=soporte_minimo, 
-            min_confidence=confianza_minima, 
-            min_lift=elevacion_minima)  
-        
-        lista_reglas = list(reglas)
-        df_reglas = pd.DataFrame(lista_reglas)
-        #se guardan las reglas en la clase 
-        apriori.setReglasAsociacion(lista_reglas)
--- back/app/app.py
+++ back/app/app.py
@@ -1,64 +1,61 @@
 from flask import Flask, request, jsonify
-from AprioriController import AprioriController
-from MetricasDistanciaController import MetricasDistanciaController
+from Apriori import Apriori
+from MetricasDistancia import MetricasDistancia
 
 ## creamos la aplicacion
 app = Flask(__name__)
-aprioriController = None
-metricasController = None
 
 @app.route('/')
 def index():
     return '¡Hola mundo!'
 
-''' 
-    ruta para agregar los parametros que utiliza el 
-    algoritmo apriori (elevacion, confianza y soporte)  
- '''
-@app.route('/api/apriori/parametros', methods=['POST'])
-def agregarParametrosApriori():
-    aprioriController = AprioriController()
-    res = aprioriController.setParametros(request)
-
-    return jsonify(res)
-
 '''
-    ruta para obtener los parametros del algoritmo apriori
-'''
-@app.route('/api/apriori/parametros', methods=['GET'])
-def getParametrosApriori():
-    aprioriController = AprioriController()
-    res = aprioriController.getParametros()
-    return jsonify(res)
-
-'''
-    ruta para obtener los resultados del algoritmo apriori
-    regresa un json con la frecuencia de todas los registros
+    ruta para obtener los resultados del algoritmo apriori,
+    recibe el archivo y los parametros y regresa un json 
+    con la frecuencia de todas los registros
     y regresa las reglas generadas
 '''
-@app.route('/api/apriori/execute', methods=['GET'])
+@app.route('/api/apriori', methods=['POST'])
 def executeApriori():
-    aprioriController = AprioriController()
-    res = aprioriController.execute()
-    return jsonify(res)
+
+    try:
+        parametros = {
+            "confianza": float(request.json['confianza']),
+            "elevacion": float(request.json['elevacion']),
+            "soporte": float(request.json['soporte'])
+        }
+
+        # file = 
+
+        res = Apriori.execute(parametros)
+        return jsonify(res)
+        
+    
+    except ValueError:
+        return {
+            "sucess": False,
+            "message": "Los parametros ['confianza', 'elevacion' y 'soporte'] deben de ser de tipo flotante."
+        }
+
+# '''
+#     ruta para agregar un valor a la metrica
+# '''
+# @app.route('/api/metricas/metrica', methods=['POST'])
+# def agregarMetrica():
+#     metricasController = MetricasDistanciaController()
+#     res = metricasController.setMetrica(request)
+#     return jsonify(res)
 
 '''
-    ruta para agregar un valor a la metrica
+    ruta para obtener las distancias, recibe el tipo de 
+    metrica y el archivo y regresa un json con los datos
+    estandarizados y la matriz de distancias
 '''
-@app.route('/api/metricas/metrica', methods=['POST'])
-def agregarMetrica():
-    metricasController = MetricasDistanciaController()
-    res = metricasController.setMetrica(request)
-    return jsonify(res)
-
-'''
-    ruta para obtener las distancias
-'''
-@app.route('/api/metricas/execute', methods=['GET'])
-def executeMetricas():
-    metricasController = MetricasDistanciaController()
-    res = metricasController.execute()
-    return jsonify(res)
+# @app.route('/api/metricas', methods=['GET'])
+# def executeMetricas():
+#     metricasController = MetricasDistanciaController()
+#     res = metricasController.execute()
+#     return jsonify(res)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
