917188b5f12d8a9944594e8627763c7cd6eb63a1
E_n_i <nestorfm801@gmail.com>
2023-05-25 02:42:04 -0600

    Tabla con las distancias obtenidas

--- front/src/components/Distancias.vue
+++ front/src/components/Distancias.vue
@@ -8,52 +8,126 @@ import Chart from 'chart.js/auto';
         data(){
             return{
                 parametros:{
-                    tipoDistancia : ''
-                }
+                    metrica : ''
+                },
+                respuesta: null,
+                col_row : 0,
+                bandera : false
+            }
+            
+        },
+
+        computed:{
+            tablaHtml(){
+                return this.generandoTabla( this.respuesta )
             }
         },
 
         methods: {
             enviandoDatos(){
                 axios.post( 'http://127.0.0.1:8000/api/metricas', this.parametros )
-                    .then( response => this.generandoMapa( response ) )
+                    .then( response => this.guardandoDatos( response ) )
                     .catch( error => console.log( error ) )
             },
 
-            generandoMapa( res ){
-                console.log( res )
+            guardandoDatos( res ){
+                this.respuesta = res.data.distancias
+                this.col_row = 5
+                this.bandera = true
+            },
+
+            generandoTabla( data ){
+                if (Array.isArray(data)) {
+
+                    const colRow = parseInt(this.col_row); // Convertir a entero
+
+                    // Formatear los datos en una tabla HTML
+                    let tabla = '<table>';
+                    tabla += '<thead><tr>';
+
+                    // Agregar encabezado de columnas
+                    tabla += '<th></th>';
+                    for (let i = 0; i < colRow; i++) {
+                        tabla += `<th>${i}</th>`;
+                    }
+
+                    tabla += '</tr></thead>';
+                    tabla += '<tbody>';
+
+                    for (let i = 0; i < colRow; i++) {
+                        tabla += '<tr>';
+                        tabla += `<td><strong>${i}</strong></td>`; // Agregar encabezado de fila
+                        for (let j = 0; j < colRow; j++) {
+                            tabla += `<td>${data[i][j]}</td>`;
+                        }
+
+                        tabla += '</tr>';
+                    }
+
+                    tabla += '</tbody>';
+                    tabla += '</table>';
+
+                    return tabla;
+                }
+
+                return '';   
             }
-
         }
-
     }
 
 </script>
 
 <template>
-
+    <h1 class="titulo">Metricas de distancias</h1>
     <form @submit.prevent="enviandoDatos">
-        <div class="contenedor">
-            <h1 class="titulo">Metricas de distancias</h1>
-        
+        <div class="contenedor">        
             <div class="selector">
-                <select name="metricas_distancias" id="distancias" v-model="parametros.tipoDistancia">
-                <option value="">Seleccione una opción</option>
-                <option value="euclidian">Euclidiana</option>
+                <label for=""><strong>Seleccione la metrica de distancia que desea utilizar:</strong></label>
+                <br>
+                <select name="metrica" id="metrica" v-model="parametros.metrica">
+                <option value="euclidean">Euclidiana</option>
                 <option value="chebyshev">Chebyshev</option>
                 <option value="cityblock">City block</option>
                 <option value="minkowski">Minkowski</option>
                 </select>
             </div>
-
-            <button type="submit" class="btn-submit">Enviar</button>
+            <div class="btn-submit">
+                <button type="submit" class="btn-submit">Enviar</button>
+            </div>
         </div>
     </form>
+
+    <!-- <div>
+        <h2>Respuesta del servidor:</h2>
+        <pre>{{ respuesta }}</pre>
+    </div> -->
+
+    <div v-if="bandera">
+        <h2>Las distancias obtenidas son las siguientes:</h2>
+        <form>
+            <div class="contenedor">        
+                <div class="selector">
+                    <label for=""><strong>Selecciones cuantas columnas y renglones desea ver:</strong></label>
+                    <br>
+                    <select name="col_row" id="" v-model="col_row">
+                    <option value="5">5</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="200">200</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+        <div v-html="tablaHtml" class="contenedor"></div>
+    </div>
+    
+
+
   </template>
   
   <style scoped>
   .titulo {
-    color: #333;
+    color: #d517cb;
     font-size: 40px;
     font-family: Arial, sans-serif;
     text-align: center;
@@ -68,6 +142,54 @@ import Chart from 'chart.js/auto';
   }
 
   .btn-submit {
-  align-self: center;
+    margin-top: 20px;
+    text-align: center;
 }
+
+.titulo {
+    color: #d517cb;
+    font-size: 40px;
+    font-family: Arial, sans-serif;
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    margin-top: 50px;
+  }
+  
+  .selector {
+    margin-top: 20px;
+    text-align: center;
+  }
+
+  .btn-submit {
+    margin-top: 20px;
+    align-self: center;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    text-align: center;
+  }
+
+  .table th,
+  .table td {
+    padding: 8px;
+    border: 1px solid #ccc;
+  }
+
+  .table th {
+    background-color: #f2f2f2;
+    text-align: center;
+  }
+
+  .table th:first-child,
+  .table td:first-child {
+    font-weight: bold;
+    text-align: center;
+  }
+
+  .table td {
+    text-align: center;
+  }
   </style>
