5253570c5fc8b134e1050349df659efd8871ebf4
Erickescorpions <erickescorpions@hotmail.com>
2023-06-04 17:14:18 -0600

    agregando visualizacion de csv

---
+++ front/src/components/CSV.vue
@@ -0,0 +1,87 @@
+<script setup>
+import axios from 'axios';
+</script>
+
+<script>
+export default {
+    props: ['file'],
+
+    data() {
+        return {
+            columnas: null,
+            data: null,
+            headers: null
+        }
+    },
+
+    methods: {
+        obteniendoDatos() {
+            const formData = new FormData();
+            formData.append('file', this.file);
+
+            if(this.columnas) {
+                let columnasNoRequeridas = [];
+                let length = this.columnas.length;
+                for(let i = 0; i < length; i++) {
+                    if(!this.columnas[i].requerida) {
+                        columnasNoRequeridas.push(this.columnas[i].columna);
+                    } 
+                }
+
+                formData.append('columnas_no_requeridas', JSON.stringify(columnasNoRequeridas));
+            }
+
+            axios.post('http://127.0.0.1:8000/api/csv', formData)
+                .then(response => {
+                    console.log(response);
+                    this.columnas = response.data.columnas;
+
+                    let length = this.columnas.length;
+                    let headers = [];
+                    for(let i = 0; i < length; i++) {
+                        if(this.columnas[i].requerida) {
+                            headers.push(this.columnas[i].columna);
+                        }
+                    }
+                    this.headers = headers;
+
+                    this.data = JSON.parse(response.data.data);
+                    console.log(this.data);
+                })
+                .catch(error => console.log(error));
+        },
+    }
+}
+</script>
+
+<template>
+    <div>
+        <v-btn variant="flat" v-if="file" v-on:click="obteniendoDatos" class="btn">
+            Mostrar Datos
+        </v-btn>
+
+        <div v-for="(item, index) in columnas">
+            <v-checkbox
+                :label="item.columna"
+                v-model="item.requerida"
+                hide-details
+                @change="obteniendoDatos"
+            ></v-checkbox>
+        </div>
+
+        <!-- Se muestra un modal -->
+        <v-table fixed-header height="500px" v-if="file">
+            <thead>
+            <tr>
+                <th class="text-left" v-for="head in headers">{{ head }}</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(row, index) in data" :key="index">
+                    <td v-for="cell in row">{{ cell }}</td>
+                </tr>
+            </tbody>
+        </v-table>
+        
+    </div>
+</template>
